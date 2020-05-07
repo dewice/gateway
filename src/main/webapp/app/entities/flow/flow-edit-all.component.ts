20dd51def08836781f84041b3c4c8345187547dc
achraf.adbib <achraf.adbib@hotmail.com>
2020-05-07 12:50:35 +0200

    Fixed warning message

--- src/main/webapp/app/entities/flow/flow-edit-all.component.ts
+++ src/main/webapp/app/entities/flow/flow-edit-all.component.ts
@@ -1024,6 +1024,7 @@ export class FlowEditAllComponent implements OnInit, OnDestroy {
                 }
             );
         }
+        this.changeDetectorRef.detectChanges();
     }
 
     setDataFromForm() {
